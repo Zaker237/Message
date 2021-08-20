06fbd8953d523238908f6b8d3e3792bfc65dbb92
Alex Mboutchouang <mboutchouangalex+github@gmail.com>
2021-08-20 04:36:38 +0200

    add README and .env.example and update the code

---
+++ README.md
@@ -0,0 +1,3 @@
+# Message
+
+this is just a small scripts, whih peak a random message from a list of message and send it to a Phone number to with a scheduled frequency.
--- message.py
+++ message.py
@@ -5,36 +5,36 @@ import time
 from twilio.rest import Client
 
 class Message():
-    def __init__(self, messages_file = "messages.txt"):
+    def __init__(self, messages_file = "./messages.txt"):
         self.messages_file = messages_file
         self.messages = []
-        
-        self.init_tokens()
-        self.load_message()
 
-    def init_tokens(self):
+        self._init_tokens()
+        self._load_message()
+
+    def _init_tokens(self):
         self.account_id = os.environ.get("TWILIO_ACCOUNT_ID")
         self.auth_token = os.environ.get("TWILIO_AUTH_ID")
         self.client = Client(self.account_id, self.auth_token)
 
     
-    def load_message(data=self.messages_file):
+    def _load_message(self, data):
         with open(data, 'r') as fl:
             message_list = fl.readlines()
             self.messages = [a[0:len(a)-1] for a in message_list]
             fl.close()
 
-    def send_message(phone_number):
+    def send_message(self, phone_number):
         choice = random.randint(0, len(self.messages)-1)
         message_body = self.messages[choice]
-        sender = '+4917674729258'
+        sender = os.environ.get("SENDER_PHONE_NUMBER")
         message = self.client.messages.create(
-            body = message_body,
-            from = sender,
-            to = phone_number
+            body=message_body,
+            from_=sender,
+            to=phone_number
         )
 
-if __name__ = "__main__"
+if __name__ == "__main__":
     message = Message()
-    phone = '+491628996477'
+    phone = os.environ.get("RECEIVER_PHONE_NUMBER")
     schedule.every().day.at("06:30").do(message.send_message, phone)
